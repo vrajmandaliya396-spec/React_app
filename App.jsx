a2c11d70cf53170de0a38d57c566398390784e99
vrajmandaliya396-spec <vrajmandaliya396@gmail.com>
2026-02-27 15:04:59 +0530

    cards play

--- App.jsx
+++ App.jsx
@@ -1,80 +1,44 @@
-import React, { useState } from 'react'
+import React, { use } from 'react'
+import Card from './Card'
+import { element } from 'three/tsl'
 
 const App = () => {
-    const eventHandler = (e) =>{
-        e.preventDefault()
-    }
+  const a = 10
 
-    const [username, setUsername] = useState("");
+  const user = [
+    {
+      "name": "Rahul Sharma",
+      "age": 25,
+      "profession": "Software Developer",
+      "profilePhoto": "images/rahul.jpg"
+    },
+    {
+      "name": "Priya Patel",
+      "age": 28,
+      "profession": "Graphic Designer",
+      "profilePhoto": "images/priya.jpg"
+    },
+    {
+      "name": "Amit Verma",
+      "age": 30,
+      "profession": "Project Manager",
+      "profilePhoto": "images/amit.jpg"
+    },
+    {
+      "name": "Neha Singh",
+      "age": 24,
+      "profession": "Frontend Developer",
+      "profilePhoto": "images/neha.jpg"
+    }
+  ]
+
+
+  
   return (
-    <div>
-      <form onSubmit={(e)=>{
-        eventHandler(e)
-        setUsername("")
-      }}>
-        <input 
-        value={username}
-        onChange={(e)=>{
-            setUsername(e.target.value)
-        }}
-        type="text" />
-        <button>Submiit</button>
-      </form>
-    </div>
+     user.map((elem)=>{
+      return <Card />
+    })
   )
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-
-// const App = () => {
-//     const [a,setA] = useState(0)
-//     const increment = () =>{
-//         setA(a+1)
-//     }
-//     const decrement = () =>{
-//         if (a > 0){
-//             setA(a-1)
-//         }else {
-//             setA(a)
-//         }
-//     }
-
-//     const [username, setUsername] = useState("")
-//     const eventHandler = (e) =>{
-//         e.preventDefault()
-//         console.log("hi")
-//         console.log(e.target)
-//     }
-
-//   return (
-//     <div>
-//       <h1>{a}</h1>
-//       <button onClick={increment}>Incrementer</button>
-//         <button onClick={decrement}> Decrementer</button>
-//         <form onSubmit={(e)=>{
-//                 eventHandler(e)
-//         }}>
-//             <input 
-//             value={(e)=>{
-//                 setUsername = e.target.value;
-//             }}
-            
-//             type="text" />
-//             <button>Submit</button>
-//         </form>
-//     </div>
-//   )
-// }
-
-// export default App
---
+++ Card.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react'
+
+const Card = (props) => {
+
+
+    
+
+  return (
+    <div>
+      <h1 >hi</h1>
+    </div>
+  )
+}
+
+export default Card
